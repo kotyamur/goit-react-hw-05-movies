bb83f9e187bdd48cd3e3cc32f0c8220648fc9e51
Kateryna Kleshch <ketrins1993@gmail.com>
2022-11-03 22:03:05 +0200

    add some Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,31 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import { Layout } from './App.styled';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
-  return <Layout>React homework template</Layout>;
+  return (
+    <Layout>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/movies">movies</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<div>home</div>} />
+        <Route path="/movies" element={<div>movies</div>} />
+        <Route path="/movies/:movieId" element={<div>movieId</div>} />
+        <Route path="*" element={<div>NotFound</div>} />
+      </Routes>
+    </Layout>
+  );
 };
